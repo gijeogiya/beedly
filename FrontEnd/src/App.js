0d809091444ed15bfaa578244d2856c606ea7ba4
ahyun98 <dldkgus98@naver.com>
2022-07-27 22:59:32 +0900

    [FRONT] ADD_Common.jsx

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {LogoHeader, NavBar, Footer} from "./components/Common"
+import { BrowserRouter,Route, Link} from "react-router-dom";
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <LogoHeader/>
+      <div className="main-content"></div>
+      <Footer />
+      
+      <NavBar />
+      
     </div>
   );
 }
