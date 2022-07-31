d1ebe1a445755070d92270ba7a2ec31bac9b3dca
ssooynn <2sy0127@gmail.com>
2022-07-31 22:00:27 +0900

    personalProductDto추가

---
+++ BackEnd/src/main/java/com/ssafy/beedly/dto/SpecialProductDto.java
@@ -0,0 +1,29 @@
+package com.ssafy.beedly.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.ssafy.beedly.domain.Category;
+import com.ssafy.beedly.domain.SpecialProductImg;
+import com.ssafy.beedly.domain.type.SoldStatus;
+
+public class SpecialProductDto {
+	private Long id;
+	private String productName;
+	private String productDesc;
+	private Integer startPrice;
+	private Integer height;
+	private Integer weight;
+	private Integer depth;
+	private SoldStatus soldStatus;
+	private Category category;
+	private List<SpecialProductImg> specialProductImgs;
+}
