8cf9a4269837d4ffb4a5f2b492f6d1a95c348b72
iamyoojincha <iamyoojincha@gmail.com>
2022-08-02 15:57:17 +0900

    [Back] : CH | AbsenteeBid 구현

--- BackEnd/src/main/java/com/ssafy/beedly/repository/query/AbsenteeBidQueryRepository.java
+++ BackEnd/src/main/java/com/ssafy/beedly/repository/query/AbsenteeBidQueryRepository.java
@@ -1,4 +1,9 @@
 package com.ssafy.beedly.repository.query;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class AbsenteeBidQueryRepository {
+    static private JPAQueryFactory queryFactory;
 }
--- BackEnd/src/main/java/com/ssafy/beedly/service/AbsenteeBidService.java
+++ BackEnd/src/main/java/com/ssafy/beedly/service/AbsenteeBidService.java
@@ -2,10 +2,13 @@ package com.ssafy.beedly.service;
 
 import com.ssafy.beedly.repository.AbsenteeBidRepository;
 import com.ssafy.beedly.repository.query.AbsenteeBidQueryRepository;
+import com.ssafy.beedly.domain.AbsenteeBid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -14,9 +17,28 @@ import org.springframework.transaction.annotation.Transactional;
 public class AbsenteeBidService {
 
     private final AbsenteeBidRepository absenteeBidRepository;
-    private final AbsenteeBidQueryRepository absenteeBidQueryRepository;
 
-    // 사전응찰 등록
+    // 서면응찰 등록
     @Transactional
-    public void save
+    public void save(AbsenteeBid absenteeBid) {
+        absenteeBidRepository.save(absenteeBid);
+    }
+
+    // 서면응찰 수정
+    @Transactional
+    public void update(AbsenteeBid absenteeBid) {
+        Optional<AbsenteeBid> bid = absenteeBidRepository.findById(absenteeBid.getId());
+
+        bid.ifPresent(selectBid -> {
+            absenteeBidRepository.save(selectBid);
+        });
+    }
+
+    @Transactional
+    // 서면응찰 삭제
+    public void delete(AbsenteeBid absenteeBid) {
+        absenteeBidRepository.deleteById(absenteeBid.getId());
+    }
+
+    // 서면응찰 목록
 }
