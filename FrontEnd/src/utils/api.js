f895acac8e27d8ab81bf072d9ef9923320472e96
zazachucky <zazachucky@naver.com>
2022-08-05 19:14:19 +0900

    [FRONT] ADD : 기획전 게시글 등록 화면, 공지사항, 마이페이지 제작

--- FrontEnd/src/utils/api.js
+++ FrontEnd/src/utils/api.js
@@ -1,1 +1,24 @@
 import axios from "axios";
+
+const API_SERVER = "http://i7a601.p.ssafy.io:8080/api/";
+
+const API_SERVER_SPECIAL = "http://i7a601.p.ssafy.io:8080/api/admin/special/";
+
+const specialApi = axios.create({
+  baseURL: API_SERVER_SPECIAL,
+  headers: {
+    "Content-type": "multipart/form-data",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMCIsImlhdCI6MTY1OTY4NTE5MSwiZXhwIjoxNjU5NzcxNTkxfQ.9mIibcLf1pe4z_wBwOBzDzyX6tHZ6t6sTaeKCwZE9CY",
+  },
+});
+
+const registerSpecialBoard = (formData, success, fail) => {
+  specialApi.post(`/board`, formData).then(success).catch(fail);
+};
+
+const getData = (params, success, fail) => {
+  specialApi.get(`/URL`, { params: params }).then(success).catch(fail);
+};
+
+export { registerSpecialBoard };
