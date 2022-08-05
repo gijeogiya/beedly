6d38ca5e43e1954f5718241b5a0b909e2e5caafa
zazachucky <zazachucky@naver.com>
2022-08-05 20:19:48 +0900

    [FRONT] ADD : Login Name changed

--- FrontEnd/src/pages/Kakao.jsx
+++ FrontEnd/src/pages/Kakao.jsx
@@ -1,26 +1,22 @@
-import React from 'react'
+import React from "react";
 import { useDispatch } from "react-redux";
 import { actionCreators as userActions } from "../stores/modules/user";
 // import { login } from '../utils/api';
 import Login from "./Login";
 
 export default function Kakao() {
-    const dispatch = useDispatch();
-    let code = new URL(window.location.href).searchParams.get("code");
-    // React.useEffect(() => {
-    //     async function fetchdata() {
-    //         await dispatch(userActions.login(code));
+  const dispatch = useDispatch();
+  let code = new URL(window.location.href).searchParams.get("code");
+  // React.useEffect(() => {
+  //     async function fetchdata() {
+  //         await dispatch(userActions.login(code));
 
-    //     }
-    //     fetchdata();
-    // }, []);
-    React.useEffect(async () => {
-        await dispatch(userActions.login(code));
-    }, []);
+  //     }
+  //     fetchdata();
+  // }, []);
+  React.useEffect(async () => {
+    await dispatch(userActions.login(code));
+  }, []);
 
-    return (
-        <Login />
-    )
+  return <Login />;
 }
-
-
