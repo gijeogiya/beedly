91f6f687f7d87b2f1c0c0dda29aae2d4dfe1785a
iamyoojincha <iamyoojincha@gmail.com>
2022-08-07 20:06:04 +0900

    [Back] : ADD | Recommendation dto add

--- BackEnd/src/main/java/com/ssafy/beedly/controller/UserRecommendationController.java
+++ BackEnd/src/main/java/com/ssafy/beedly/controller/UserRecommendationController.java
@@ -3,14 +3,12 @@ package com.ssafy.beedly.controller;
 import com.ssafy.beedly.config.web.LoginUser;
 import com.ssafy.beedly.domain.RecommendationTag;
 import com.ssafy.beedly.domain.User;
+import com.ssafy.beedly.dto.SelectedTagsDto;
 import com.ssafy.beedly.service.UserRecommendationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,8 +19,13 @@ public class UserRecommendationController {
     private final UserRecommendationService userRecommendationService;
 
     @PostMapping
-    public ResponseEntity<?> saveUserRecommendationTag(@LoginUser User user, @RequestBody List<Long> tags) {
-        userRecommendationService.add(user, tags);
+    public ResponseEntity<?> saveUserRecommendationTag(@LoginUser User user, @RequestBody SelectedTagsDto selectedTagsDto ) {
+        userRecommendationService.add(user, selectedTagsDto.getTags());
         return ResponseEntity.status(HttpStatus.OK).build();
     }
+
+//    @PostMapping
+//    public void getSelectedTags(@RequestBody SelectedTagsDto tags) {
+//        asdasdasda
+//    }
 }
