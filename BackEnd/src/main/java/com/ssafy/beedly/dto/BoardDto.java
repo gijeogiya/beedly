6448f646e5249863cf714e573977736167a0f52b
iamyoojincha <iamyoojincha@gmail.com>
2022-08-08 17:58:07 +0900

    [Back] : CH | BoardDto updated

--- BackEnd/src/main/java/com/ssafy/beedly/dto/BoardDto.java
+++ BackEnd/src/main/java/com/ssafy/beedly/dto/BoardDto.java
@@ -1,4 +1,21 @@
 package com.ssafy.beedly.dto;
 
+import com.ssafy.beedly.domain.Board;
+import com.ssafy.beedly.domain.User;
+import com.ssafy.beedly.domain.type.BoardType;
+
 public class BoardDto {
+    private Long id;
+    private String boardTitle;
+    private String boardContent;
+    private BoardType boardType;
+    private User user;
+
+    public BoardDto(Board board) {
+        this.id = board.getId();
+        this.boardTitle = board.getBoardTitle();
+        this.boardContent = board.getBoardContent();
+        this.boardType = board.getBoardType();
+        this.user = board.getUser();
+    }
 }
