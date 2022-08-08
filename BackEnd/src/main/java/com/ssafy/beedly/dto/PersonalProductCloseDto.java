1f2a0044f43af22ac2abc080b9030028058c25bc
rjeowornjs2 <rjeowornjs@naver.com>
2022-08-08 17:08:09 +0900

    [Back] : UPDATE | PersonalProduct 관련 Swagger 작성

--- BackEnd/src/main/java/com/ssafy/beedly/dto/PersonalProductCloseDto.java
+++ BackEnd/src/main/java/com/ssafy/beedly/dto/PersonalProductCloseDto.java
@@ -3,6 +3,7 @@ package com.ssafy.beedly.dto;
 import com.ssafy.beedly.domain.AbsenteeBid;
 import com.ssafy.beedly.domain.PersonalFavorite;
 import com.ssafy.beedly.domain.SearchTag;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,10 +13,20 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 public class PersonalProductCloseDto {
+
+    @ApiModelProperty(notes = "상시 상품 식별자")
     private Long productId;
+
+    @ApiModelProperty(notes = "태그 이름들")
     private List<String> tagNames;
+
+    @ApiModelProperty(notes = "서면 응찰 가격")
     private Integer absenteeBidPrice;
+
+    @ApiModelProperty(notes = "서면 응찰 여부")
     private Boolean isAbsenteeBid;
+
+    @ApiModelProperty(notes = "상품 찜 여부")
     private Boolean isFavorite;
 
     public PersonalProductCloseDto(Long productId, List<String> tagNames, Integer absenteeBidPrice,Boolean isAbsenteeBid ,Boolean isFavorite) {
