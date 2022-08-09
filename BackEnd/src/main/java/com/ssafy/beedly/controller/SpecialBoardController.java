c9e2f75d5d30d87803e894e37552d617cb089821
ssooynn <2sy0127@gmail.com>
2022-08-10 02:16:48 +0900

    API 설명 추가

--- BackEnd/src/main/java/com/ssafy/beedly/controller/SpecialBoardController.java
+++ BackEnd/src/main/java/com/ssafy/beedly/controller/SpecialBoardController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import springfox.documentation.annotations.ApiIgnore;
 
-import javax.xml.ws.Response;
 import java.util.List;
 
 @RestController
