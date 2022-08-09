770b86d37bce812e39b8bc8b91c1724f9e915006
iamyoojincha <iamyoojincha@gmail.com>
2022-08-09 17:58:11 +0900

    [Back] : CH | Notice & FAQ CRUD

--- BackEnd/src/main/java/com/ssafy/beedly/repository/BoardRepository.java
+++ BackEnd/src/main/java/com/ssafy/beedly/repository/BoardRepository.java
@@ -1,4 +1,18 @@
 package com.ssafy.beedly.repository;
 
-public interface BoardRepository {
+import com.ssafy.beedly.domain.Board;
+import com.ssafy.beedly.domain.type.BoardType;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    @Query(value = "select b from Board b where b.boardType = :boardType")
+    List<Board> findBoardByType(@Param("boardType") BoardType boardType);
+
 }
