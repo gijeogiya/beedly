1ad287af98d17b94d77ef244b447bb0bd1a3fd11
ssooynn <2sy0127@gmail.com>
2022-08-10 11:32:29 +0900

    AdminController 수정

--- BackEnd/src/main/java/com/ssafy/beedly/controller/AdminController.java
+++ BackEnd/src/main/java/com/ssafy/beedly/controller/AdminController.java
@@ -1,5 +1,7 @@
 package com.ssafy.beedly.controller;
 
+import com.ssafy.beedly.config.web.LoginUser;
+import com.ssafy.beedly.domain.User;
 import com.ssafy.beedly.service.AdminService;
 
 import io.swagger.annotations.ApiImplicitParam;
@@ -19,17 +21,16 @@ public class AdminController {
 
     // 1. 작가 신청 받기
     @ApiOperation(value = "작가 신청서 받기", notes = "작가 신청서를 받아서 artist승인 테이블에 승인 여부 false로 저장")
-    @ApiImplicitParam(name = "userId", value = "유저아이디", paramType = "path", dataType = "Long")
-    @GetMapping("/application/{artistId}")
-    ResponseEntity<?> setArtistApplication(@PathVariable Long userId){
-        adminService.saveArtistApplication(userId);
+    @GetMapping("/application}")
+    ResponseEntity<?> setArtistApplication(@LoginUser User user){
+        adminService.saveArtistApplication(user.getId());
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     // 2. 작가 등급 올려주기
     @ApiOperation(value = "작가 승인해서 등급올리기", notes = "작가 승인 한 후에, 등급 올리고 artist테이블에 정보 추가")
     @ApiImplicitParam(name = "userId", value = "유저아이디", paramType = "path", dataType = "Long")
-    @PatchMapping("updgrade/{artistId}")
+    @PatchMapping("updgrade/{userId}")
     ResponseEntity<?> upgradeArtist(@PathVariable Long userId){
         adminService.upgradeArtist(userId);
         return ResponseEntity.status(HttpStatus.OK).build();
