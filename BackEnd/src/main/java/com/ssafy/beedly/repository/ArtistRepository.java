d7669f5af70f43270b5abe3d4b2733f3737475fd
이수연 <2sy0127@naver.com>
2022-08-14 23:03:00 +0900

    Revert "예술작가 마이페이지에서 상품가져오기"
    This reverts commit 273bbcf031c70babfccddf3d68e7e9caa7a1211f

--- BackEnd/src/main/java/com/ssafy/beedly/repository/ArtistRepository.java
+++ BackEnd/src/main/java/com/ssafy/beedly/repository/ArtistRepository.java
@@ -1,8 +1,6 @@
 package com.ssafy.beedly.repository;
 
 import com.ssafy.beedly.domain.Artist;
-import com.ssafy.beedly.domain.PersonalProduct;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -15,12 +13,4 @@ public interface ArtistRepository extends JpaRepository<Artist, Long> {
 
     // userId로 작가 조회
     Optional<Artist> findArtistByUserId(Long userId);
-
-    // 작가의 진행중인 작품"
-    @Query("select p from PersonalProduct p where p.artist.id = :userId and p.soldStatus='SUCCESS'")
-    Slice<PersonalProduct> findEndProductByArtistId(Long userId);
-
-    @Query("select p from PersonalProduct p where p.artist.id = :userId and p.soldStatus='STANDBY'")
-    Slice<PersonalProduct> findStandByProductByArtistId(Long userId);
-
 }
