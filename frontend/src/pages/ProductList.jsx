24e8e1fffc59d35bbe16587da537527f15c10ff6
Gijeong <mag012525@gmail.com>
2022-08-15 00:10:58 +0900

    [Front] Add_ProductList

--- frontend/src/pages/ProductList.jsx
+++ frontend/src/pages/ProductList.jsx
@@ -2,10 +2,58 @@ import { Box } from "grommet";
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { CategoryBar } from "../components/MainCategoryBar";
+import styled from "styled-components";
+import { HorizonScrollRowTable } from "../components/HorizonScrollTable";
+import { SortButtonArea } from "../components/SortButtonArea"
+import { ProductCard } from '../components/ProductCard'
+import { getOnairApi, getPersonalProductListApi } from "../utils/api";
+
+
+const StyledTableTitle = styled.div`
+  font-size: 16px;
+  color: #1f1d1d;
+  padding-left: 20px;
+  font-weight: 900;
+`;
+
+const StyledTableSubtitle = styled.div`
+  font-size: 14px;
+  padding-left: 20px;
+  font-weight: 500;
+  color: rgba(31, 29, 29, 0.4);
+  `;
 
 
 export default function ProductListPage() {
   const navigate = useNavigate();
+  const [OnairList, setOnairList] = useState([]);
+  const [NewProductList, setNewProductList] = useState([]);
+  const [loading, setloading] = useState(true);
+
+  const [Size, setSize] = useState({});
+  useEffect(() => {
+    if (loading) {
+
+      // 진행중인 경매
+      getOnairApi("0", "20", "", (res) => {
+        console.log(res);
+        setOnairList(res.data.content);
+      }, (err) => {
+        console.log(err);
+      })
+
+      //신규 작품 목록 가져오기
+      getPersonalProductListApi("0", "20", "createdDate,DESC", (res) => {
+        setNewProductList(res.data.content);
+        console.log(res.data);
+      }, (err) => {
+        console.log(err);
+      })
+
+      setloading(false);
+    }
+    // eslint-disable-next-line
+  }, [loading]);
 
   const purchaseTest = () => {
     navigate(`/purchase/27`, {
@@ -16,9 +64,51 @@ export default function ProductListPage() {
       },
     });
   };
+
+  const ProductTable = styled.div`
+      justify-content: space-between;
+      display: flex;
+      flex-wrap: wrap;
+  `;  
+  
   return (
     <div>
       <CategoryBar/>
+      <div style={{
+        borderBottom: "1px solid #ebebeb",
+        padding: "20px 0",
+      }}>
+        <StyledTableTitle>On Air</StyledTableTitle>
+        <StyledTableSubtitle>지금 진행중인 개인경매</StyledTableSubtitle>
+        <HorizonScrollRowTable list={OnairList} />
+      </div>
+      <div
+        style={{
+          borderBottom: "2px solid #ebebeb",
+          padding: "20px 0",
+        }}>
+        <StyledTableTitle>New In</StyledTableTitle>
+        <StyledTableSubtitle>신규 등록 작품</StyledTableSubtitle>
+        <HorizonScrollRowTable list={NewProductList} />
+      </div>
+      <div
+        style={{
+          borderBottom: "2px solid #ebebeb",
+          padding: "20px 0",
+        }}>
+        <StyledTableTitle>Products</StyledTableTitle>
+        <StyledTableSubtitle>개인 경매 예정 작품</StyledTableSubtitle>
+        <SortButtonArea/>
+          <div style={{display:"flex", justifyContent:"center", padding: "0 5vw"}}>
+            <ProductTable>
+              <ProductCard/>
+              <ProductCard/>
+              <ProductCard/>
+              <ProductCard/>
+              <ProductCard/>
+            </ProductTable>
+          </div>
+      </div>
       
     </div>
     // <Box margin="small">
