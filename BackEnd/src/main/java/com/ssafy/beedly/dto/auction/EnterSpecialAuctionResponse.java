70fbc838ba17fabe3dcc27d40ac2cab9f4555006
rjeowornjs2 <rjeowornjs@naver.com>
2022-08-15 01:10:25 +0900

    [Back] : UPDATE | 기획전 입장, 낙찰확정 때 현재 상품 인덱스 기능 추가

--- BackEnd/src/main/java/com/ssafy/beedly/dto/auction/EnterSpecialAuctionResponse.java
+++ BackEnd/src/main/java/com/ssafy/beedly/dto/auction/EnterSpecialAuctionResponse.java
@@ -2,56 +2,28 @@ package com.ssafy.beedly.dto.auction;
 
 import com.ssafy.beedly.domain.SpecialAuction;
 import com.ssafy.beedly.domain.SpecialProduct;
-import com.ssafy.beedly.domain.SpecialProductImg;
-import com.ssafy.beedly.dto.category.CategoryDto;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class EnterSpecialAuctionResponse implements Serializable {
+public class EnterSpecialAuctionResponse {
 
-    @ApiModelProperty(notes = "기획전 경매방 식별자")
-    private Long auctionId;
+    private List<SpecialAuctionResponse> specialAuctionResponses = new ArrayList<>();
+    private Integer curSProdIdx;
 
-    @ApiModelProperty(notes = "상품 식별자")
-    private Long productId;
-
-    @ApiModelProperty(notes = "상품 이름")
-    private String productName;
-
-    @ApiModelProperty(notes = "상품 설명")
-    private String productDesc;
-
-    @ApiModelProperty(notes = "시작 가격")
-    private Integer startPrice;
-
-    @ApiModelProperty(notes = "작가 이름")
-    private String artistName;
-
-    @ApiModelProperty(notes = "상품 이미지들")
-    private List<String> productImages = new ArrayList<>();
-
-    private CategoryDto categoryDto;
-
-    public EnterSpecialAuctionResponse(Long auctionId, SpecialProduct sp) {
-        this.auctionId = auctionId;
-        this.productId = sp.getId();
-        this.productName = sp.getProductName();
-        this.productDesc = sp.getProductDesc();
-        this.startPrice = sp.getStartPrice();
-        this.artistName = sp.getArtistName();
-        List<SpecialProductImg> images = sp.getSpecialProductImgs();
-        for (SpecialProductImg image : images) {
-            this.productImages.add(image.getImgUri());
-        }
-        this.categoryDto = new CategoryDto(sp.getCategory());
+    public EnterSpecialAuctionResponse(SpecialAuction specialAuction) {
+        List<SpecialProduct> specialProducts = specialAuction.getSpecialBoard().getSpecialProducts();
+        this.specialAuctionResponses = specialProducts
+                .stream()
+                .map(specialProduct -> new SpecialAuctionResponse(specialAuction.getId(), specialProduct))
+                .collect(Collectors.toList());
+        this.curSProdIdx = specialAuction.getCurSProdIdx();
     }
 }
