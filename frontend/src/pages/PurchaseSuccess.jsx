4c75044af2a74b5db705d16b07f9545fa21468c8
ahyun98 <dldkgus98@naver.com>
2022-08-18 16:43:21 +0900

    [FRONT] fix Css

--- frontend/src/pages/PurchaseSuccess.jsx
+++ frontend/src/pages/PurchaseSuccess.jsx
@@ -20,26 +20,27 @@ export const PurchaseSuccess = () => {
 
   const [loading, setLoading] = useState(true);
 
-  const [userAddr, setUserAddr] = useState();
-  const [userName, setUserName] = useState();
-  const [userTel, setUserTel] = useState();
-  const [finalPrice, setFinalPrice] = useState();
-  const [paidFlag, setPaidFlag] = useState();
-  const [productName, setProductName] = useState();
+  const [userAddr, setUserAddr] = useState("");
+  const [userName, setUserName] = useState("");
+  const [userTel, setUserTel] = useState("");
+  const [finalPrice, setFinalPrice] = useState("");
+  const [paidFlag, setPaidFlag] = useState(true);
+  const [productName, setProductName] = useState("");
   const [product, setProduct] = useState({});
-  const [userEmail, setUserEmail] = useState();
+  const [userEmail, setUserEmail] = useState("");
   const navigate = useNavigate();
+
   useEffect(() => {
     setProduct(
       (prev) =>
-        (prev = {
-          name: "",
-          categoryId: "",
-          artist: "",
-          desc: "",
-          tag: [],
-          images: [],
-        })
+      (prev = {
+        name: "",
+        categoryId: "",
+        artist: "",
+        desc: "",
+        tags: [],
+        images: [],
+      })
     );
     if (loading) getPurchaseInfo();
     return () => setLoading(false);
@@ -160,14 +161,14 @@ export const PurchaseSuccess = () => {
             let data = response.data.personalProductDto;
             setProduct(
               (prev) =>
-                (prev = {
-                  name: data.productName,
-                  categoryId: data.categoryId,
-                  artist: data.userName,
-                  desc: data.productDesc,
-                  tag: data.tagNames,
-                  images: [...data.productImgs],
-                })
+              (prev = {
+                name: data.productName,
+                categoryId: data.categoryId,
+                artist: data.userName,
+                desc: data.productDesc,
+                tags: data.tagNames,
+                images: [...data.productImgs],
+              })
             );
             setLoading(false);
           },
@@ -198,7 +199,7 @@ export const PurchaseSuccess = () => {
           </Box>
         )}
         {/* 상품 화면 */}
-        <Box>
+        <Box direction="column" align="center">
           {/* <MainImg src={Product1} /> */}
           <Carousel
             fill
@@ -210,81 +211,75 @@ export const PurchaseSuccess = () => {
               return <Image src={image} fit="cover" key={idx} />;
             })}
           </Carousel>
-          <Box margin="medium">
-            <StyledText
-              text="낙찰 정보"
-              weight="bold"
-              size="20px"
-              style={{ textDecoration: "underline" }}
-            />
-            <StyledText
-              text={Category[product.categoryId].label}
-              weight="bold"
-              size="16px"
-              style={{ textDecoration: "underline" }}
-            />
-            <StyledText text={product.artist} />
-            <StyledText text={product.name} />
+          <Box width="85vw" >
+
+            <Box margin="small" direction="column" height="80px" justify="between" style={{ marginTop: "20px" }}>
+              <StyledText
+                text="낙찰 정보"
+                weight="bold"
+                size="15px"
+                style={{ paddingBottom: "20px" }}
+              />
+
+
+              <StyledText text={product.name} size="20px" weight="bold" />
+              <StyledText text={product.artist} size="16px" />
+
+
+            </Box>
+
+            <Box margin="small" style={{ marginTop: "30px" }}>
+              <StyledText text="작품 설명" size="15px" style={{ margin: "5px 0px" }} />
+              <StyledText text={product.desc} size="14px" color="#7B7B7B" />
+            </Box>
+            <Box direction="row" justify="between" margin="small">
+              <StyledText text="낙찰 금액" size="15px" />
+              <StyledText text={`${moneyFormat(finalPrice)}원`} />
+            </Box>
+            <Box margin="small" direction="column" height="400px" justify="between" style={{ marginTop: "50px" }}>
+              <StyledText text="결제 정보" weight="bold" size="15px" />
+              <Box direction="row" justify="between" margin={{ bottom: "30px" }}>
+                <StyledText text="이름" weight="bold" />
+                <StyledText text={userName} />
+              </Box>
+              <Box direction="row" justify="between">
+                <StyledText text="전화번호" weight="bold" />
+                <StyledText text={userTel} />
+              </Box>
+              <Box>
+                <StyledText text="주소" weight="bold" style={{ marginBottom: "8px" }} />
+                <StyledText text={userAddr} />
+              </Box>
+              <Box direction="column" justify="between" height="130px" align="end">
+                <StyledText
+                  text={`수수료(5%) ${moneyFormat(parseInt(finalPrice * 0.05))}`}
+                />
+
+                <StyledText text={`배송비 4,000`} />
+                <Box direction="row" justify="between" align="end" width="80vw">
+                  <StyledText text="총 결제금액" weight="bold" />
+                  <StyledText
+                    text={`${moneyFormat(parseInt(finalPrice * 1.05) + 4000)}원`}
+                    weight="bold"
+                    size="20px"
+                    style={{ color: "red" }}
+                  />
+                </Box>
+                <StyledHr width="100%" color="#A1A1A1" style={{ height: "2px" }} />
+
+                <StyledText text={paidFlag ? "결제 완료" : "결제 대기중"} />
+              </Box>
+            </Box>
+            <Box direction="row" justify="center">
+              {!paidFlag && (
+                <Button
+                  BigYellow
+                  children="카카오페이로 결제하기"
+                  onClick={onClickPayMent}
+                />
+              )}
+            </Box>
           </Box>
-          <Box margin="small">
-            <StyledText text="작품 설명" />
-            <StyledText text={product.desc} size="10px" color="#7B7B7B" />
-          </Box>
-          <Box margin="small">
-            <StyledText text="작품 정보" />
-            <div>태그들 태그들</div>
-          </Box>
-          <Box margin="small">
-            <StyledText text="낙찰 금액" />
-            <StyledText text={`${moneyFormat(finalPrice)}원`} />
-          </Box>
-        </Box>
-        <Box margin="medium">
-          <StyledText text="결제 정보" weight="bold" size="20px" />
-          <StyledText text="배송지정보" weight="bold" size="16px" />
-          <Box>
-            <StyledText text="주소" />
-            <StyledText text={userAddr} />
-            <StyledHr width="100%" color="#A1A1A1" />
-          </Box>
-          <Box>
-            <StyledText text="이름" />
-            <StyledText text={userName} />
-            <StyledHr width="100%" color="#A1A1A1" />
-          </Box>
-          <Box>
-            <StyledText text="전화번호" />
-            <StyledText text={userTel} />
-            <StyledHr width="100%" color="#A1A1A1" />
-          </Box>
-          <Box>
-            <StyledText
-              text={`수수료(5%) ${moneyFormat(parseInt(finalPrice * 0.05))}`}
-            />
-            <StyledText text={`배송비 4,000`} />
-            <StyledText text="총 결제금액" />
-            <StyledText
-              text={`${moneyFormat(parseInt(finalPrice * 1.05) + 4000)}원`}
-              weight="bold"
-              size="20px"
-            />
-            <StyledHr width="100%" color="#A1A1A1" />
-            <StyledText text={paidFlag ? "결제 완료" : "결제 대기중"} />
-          </Box>
-        </Box>
-        <Box direction="row" justify="center">
-          {!paidFlag && (
-            <Button
-              BigYellow
-              children="카카오페이로 결제하기"
-              onClick={onClickPayMent}
-            />
-          )}
-          {/* <Button
-            BigYellow
-            children="카카오페이로 결제하기"
-            onClick={onClickPayMent}
-          /> */}
         </Box>
       </Box>
     );
