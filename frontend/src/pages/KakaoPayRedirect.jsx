dc6f1a2947ca9a6547055a3c1b227964620dd36d
zazachucky <zazachucky@naver.com>
2022-08-23 14:07:56 +0900

    [FRONT] 카카오페이 결제 개선(로그인 수정 필요!!)

--- frontend/src/pages/KakaoPayRedirect.jsx
+++ frontend/src/pages/KakaoPayRedirect.jsx
@@ -1,16 +1,14 @@
 import { Box, Spinner } from "grommet";
 import React, { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 import Button from "../components/Button";
 import { StyledText } from "../components/Common";
 import { postPersonalPay, postSpecialPay } from "../utils/apis/PayAPI";
 
 export const KakaoPayRedirect = () => {
-  const params = new URL(window.location.href).searchParams;
+  const [params, setParams] = useSearchParams();
+  // const params = new URL(window.location.href).searchParams;
   const [loading, setLoading] = useState(true);
-  const location = useLocation();
-  const [user, setUser] = useState();
-  const [product, setProduct] = useState();
   const navigate = useNavigate();
   let success = params.get("imp_success");
   let errorMsg = params.get("error_msg");
@@ -19,7 +17,6 @@ export const KakaoPayRedirect = () => {
   let auctionType = merchantUid.split("_")[0];
   // auctionType + "_order_no_" + soldId,
   useEffect(() => {
-    console.log(params);
     console.log(success + ", " + errorMsg + ", " + soldId, auctionType);
     if (loading)
       if (success === "true" || success === true) {
@@ -29,15 +26,6 @@ export const KakaoPayRedirect = () => {
             (response) => {
               setLoading(false);
               console.log("결제 완료 API 호출 !!!", response);
-              navigate(`/purchaseDetail/${soldId}`, {
-                state: {
-                  error_msg: errorMsg,
-                  merchant_uid: merchantUid,
-                  success: success,
-                  soldId: soldId,
-                  auctionType: auctionType,
-                },
-              });
             },
             (fail) => {
               console.log(fail);
@@ -49,21 +37,21 @@ export const KakaoPayRedirect = () => {
             (response) => {
               setLoading(false);
               console.log("결제 완료 API 호출 !!!", response);
-              navigate(`/purchaseDetail/${soldId}`, {
-                state: {
-                  error_msg: errorMsg,
-                  merchant_uid: merchantUid,
-                  success: success,
-                  soldId: soldId,
-                  auctionType: auctionType,
-                },
-              });
             },
             (fail) => {
               console.log(fail);
             }
           );
-      } else {
+        navigate(`/purchaseDetail/${soldId}`, {
+          state: {
+            error_msg: errorMsg,
+            merchant_uid: merchantUid,
+            success: success,
+            soldId: soldId,
+            auctionType: auctionType,
+          },
+        });
+      } else if (success === "false" || success === false) {
         setLoading(false);
       }
     return () => setLoading(false);
